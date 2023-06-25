cce60ef92e5ba32339c24ab816e44cb65cabdaf0
Erkapos <playstasiun@gmail.com>
2023-06-25 22:42:51 +0700

    added hamburger icon

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,9 +2,15 @@ import "./navbar.css";
 import albaLogo from "./images/alba-logo.svg";
 import ReusableButton from "../reusable-button/reusable-button";
 import { useNavigate } from "react-router-dom";
+import hamburger from "./images/hamburger-icon.svg";
 
 function Navbar() {
   const navigate = useNavigate();
+
+  function toggleMenu() {
+    var navbarButtons = document.querySelector(".navbar-menu-list");
+    navbarButtons.classList.toggle("hidden");
+  }
 
   return (
     <div className="navbar-container">
@@ -14,12 +20,50 @@ function Navbar() {
         }}
         src={albaLogo}
       />
-      <p className="navbar-item hidden">Service</p>
-      <p className="navbar-item hidden">About</p>
-      <p className="navbar-item hidden">Portfolio</p>
-      <p className="navbar-item hidden">Blog</p>
-      <p className="navbar-item hidden">Career</p>
-      <ReusableButton title="Contact Us" isFilled={true} />
+
+      <div class="navbar-buttons reverse-hidden">
+        <ul class="navbar-menu-list hidden">
+          <li>
+            <a className="m-font" href="#">
+              Service
+            </a>
+          </li>
+          <li>
+            <a className="m-font" href="#">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="m-font" href="#">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a className="m-font" href="#">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a className="m-font" href="#">
+              Career
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <ReusableButton
+        additionalClass="hidden"
+        title="Contact Us"
+        isFilled={true}
+      />
+
+      <img
+        onClickCapture={toggleMenu}
+        class="reverse-hidden"
+        id="hamburger-button"
+        src={hamburger}
+        alt="hamburger-icon"
+      />
     </div>
   );
 }
--- src/components/reusable-button/reusable-button.jsx
+++ src/components/reusable-button/reusable-button.jsx
@@ -2,9 +2,16 @@ import "./reusable-button.css";
 
 function ReusableButton(props) {
   return props.isFilled ? (
-    <button onClick={props.onClick} className="reusable-button">{props.title}</button>
+    <button
+      onClick={props.onClick}
+      className={"reusable-button " + props.additionalClass}
+    >
+      {props.title}
+    </button>
   ) : (
-    <button className="reusable-button-unfilled">{props.title}</button>
+    <button className={"reusable-button-unfilled " + props.additionalClass}>
+      {props.title}
+    </button>
   );
 }
 
